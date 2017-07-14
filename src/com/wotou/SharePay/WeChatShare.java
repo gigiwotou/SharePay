4440ec78e35b3ac6103a87d67c9e134e81eb11a4
gigiwotou <gigiwotou@sohu.com>
2017-07-14 17:47:32 +0800

    1
    1

--- src/com/wotou/SharePay/WeChatShare.java
+++ src/com/wotou/SharePay/WeChatShare.java
@@ -11,6 +11,8 @@ import com.unity3d.player.UnityPlayerActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class WeChatShare extends UnityPlayerActivity
 {
@@ -22,6 +24,25 @@ public class WeChatShare extends UnityPlayerActivity
 	public void onCreate(Bundle savedInstanceState) 
 	{
 	    super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 	
 	public String initappid(String callbackGameObject,String appid, int platfrom)
